18505acf53cf0f54478619bfbfd8472d976bc7c8
krish <rokrish@calpoly.edu>
2026-02-12 23:44:29 -0800

    set up a3

---
+++ main.py
@@ -0,0 +1,9 @@
+from typing import *
+from dataclasses import dataclass
+import unittest
+import sys
+sys.setrecursionlimit(10**6)
+class Tests(unittest.TestCase):
+    pass
+if (__name__ == '__main__'):
+    unittest.main()
